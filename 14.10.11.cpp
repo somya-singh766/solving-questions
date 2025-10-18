8334a8056a747c84a3bfd8e165b5ef0a4c094364
somya-singh766 <Somyasingh3007@gmail.com>
2025-10-18 16:56:45 +0530

    Update 14.10.11.cpp

--- 14.10.11.cpp
+++ 14.10.11.cpp
@@ -1,1 +1,13 @@
+int removeDuplicates(vector<int> &arr, int n) {
+	// Write your code here.
+	 if (n == 0) return 0;
 
+        int i = 0; // index of last unique element
+        for (int j = 1; j < n; j++) {
+            if (arr[j] != arr[i]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+        return i + 1;
+}
