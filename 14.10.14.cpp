9f4e7124344c064e2ad26d23eb215826ac4c5032
somya-singh766 <Somyasingh3007@gmail.com>
2025-10-18 17:04:45 +0530

    Update 14.10.14.cpp

--- 14.10.14.cpp
+++ 14.10.14.cpp
@@ -1,1 +1,38 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> result;
+    int top = 0;
+    int bottom = matrix.size() - 1;
+    int left = 0;
+    int right = matrix[0].size() - 1;
 
+    while (top <= bottom && left <= right) {
+        // Step 1: left → right
+        for (int i = left; i <= right; i++)
+            result.push_back(matrix[top][i]);
+        top++;
+
+        // Step 2: top → bottom
+        for (int i = top; i <= bottom; i++)
+            result.push_back(matrix[i][right]);
+        right--;
+
+        // Step 3: right → left
+        if (top <= bottom) {
+            for (int i = right; i >= left; i--)
+                result.push_back(matrix[bottom][i]);
+            bottom--;
+        }
+
+        // Step 4: bottom → top
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--)
+                result.push_back(matrix[i][left]);
+            left++;
+        }
+    }
+
+    return result;
+    }
+};
