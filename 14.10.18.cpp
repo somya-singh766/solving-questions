6259790742449c4ef5f34589c68417c42010acb3
somya-singh766 <Somyasingh3007@gmail.com>
2025-10-18 17:14:39 +0530

    Update 14.10.18.cpp

--- 14.10.18.cpp
+++ 14.10.18.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+        int maxCount = 0;
+    int rowIndex = 0;
 
+    for (int i = 0; i < mat.size(); i++) {
+        int count = 0;
+        for (int j = 0; j < mat[i].size(); j++) {
+            if (mat[i][j] == 1)
+                count++;
+        }
+
+        if (count > maxCount) {
+            maxCount = count;
+            rowIndex = i;
+        }
+        // If count == maxCount, we keep the smaller index (do nothing)
+    }
+
+    return {rowIndex, maxCount};
+    }
+};
