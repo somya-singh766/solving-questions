c9bba82a15e13f2e1a5d04ec7d0a66943244f25f
somya-singh766 <Somyasingh3007@gmail.com>
2025-10-18 17:01:51 +0530

    Update 14.10.13.cpp

--- 14.10.13.cpp
+++ 14.10.13.cpp
@@ -1,1 +1,9 @@
-
+class Solution {
+  public:
+    int towerOfHanoi(int n, int from, int to, int aux) {
+        // code here
+        if (n == 0)
+        return 0;
+    return pow(2, n) - 1; 
+    }
+};
