bded2e86036a37a7a18a03367ed4ebd5940e405f
somya-singh766 <Somyasingh3007@gmail.com>
2025-10-19 09:35:04 +0530

    Update 15.10.15.cpp

--- 15.10.15.cpp
+++ 15.10.15.cpp
@@ -1,1 +1,32 @@
+class Solution {
+  public:
+    int evaluatePostfix(vector<string>& arr) {
+        // code here
+        stack<int> st;
 
+    for (string token : arr) {
+        if (token == "+" || token == "-" || token == "*" || token == "/" || token == "^") {
+            int b = st.top(); st.pop();
+            int a = st.top(); st.pop();
+            int res = 0;
+
+            if (token == "+") res = a + b;
+            else if (token == "-") res = a - b;
+            else if (token == "*") res = a * b;
+            else if (token == "/")
+               if (a * b < 0 && a % b != 0)
+                  res = a / b - 1;
+               else
+                  res = a / b; // integer division
+            else if (token == "^") res = pow(a, b); // exponentiation
+
+            st.push(res);
+        } else {
+            // Convert string to int and push
+            st.push(stoi(token));
+        }
+    }
+
+    return st.top();
+    }
+};
