967b098f1d9930a7a301bacf903eaf260b151463
somya-singh766 <Somyasingh3007@gmail.com>
2025-10-19 09:31:10 +0530

    Update 15.10.13.cpp

--- 15.10.13.cpp
+++ 15.10.13.cpp
@@ -1,1 +1,19 @@
+vector<int> Smallestonleft(int arr[], int n) {
+    // code here
+    vector<int> ans(n, -1);
+    stack<int> st;
 
+    for (int i = 0; i < n; i++) {
+        while (!st.empty() && st.top() >= arr[i])
+            st.pop();
+
+        if (!st.empty())
+            ans[i] = st.top();
+        else
+            ans[i] = -1;
+
+        st.push(arr[i]);
+    }
+
+    return ans;
+}
