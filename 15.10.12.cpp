97a5ae3b27f9c47d11f3ec60c652f49adb633a78
somya-singh766 <Somyasingh3007@gmail.com>
2025-10-19 09:20:43 +0530

    Update 15.10.12.cpp

--- 15.10.12.cpp
+++ 15.10.12.cpp
@@ -1,1 +1,33 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& findNums, vector<int>& nums) {
+        unordered_map<int, int> nextGreater;
+        stack<int> st;
 
+       
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            int num = nums[i];
+
+            
+            while (!st.empty() && st.top() <= num)
+                st.pop();
+
+            
+            if (st.empty())
+                nextGreater[num] = -1;
+            else
+                nextGreater[num] = st.top();
+
+            
+            st.push(num);
+        }
+
+        
+        vector<int> result;
+        for (int num : findNums) {
+            result.push_back(nextGreater[num]);
+        }
+
+        return result;
+    }
+};
