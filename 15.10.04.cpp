a999995f922fcc6837c71393bde45a81edf97ca9
somya-singh766 <Somyasingh3007@gmail.com>
2025-10-19 08:57:18 +0530

    Update 15.10.04.cpp

--- 15.10.04.cpp
+++ 15.10.04.cpp
@@ -1,1 +1,27 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *next;
 
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int getCount(Node* head) {
+        // Code here
+        int count = 0;
+    Node* temp = head;
+
+    while (temp != nullptr) {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+    }
+};
