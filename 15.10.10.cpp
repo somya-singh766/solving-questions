3996a785ff643ea0e168b7c4f8efccf860b8db7d
somya-singh766 <Somyasingh3007@gmail.com>
2025-10-19 09:10:19 +0530

    Update 15.10.10.cpp

--- 15.10.10.cpp
+++ 15.10.10.cpp
@@ -1,1 +1,25 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* iter = head;
+        while (iter) {
+            ListNode* run = iter->next;
+            while (run && run->val == iter->val) {
+                run = run->next;
+            }
+            iter->next = run;
+            iter = run;
+        }
+        return head; 
+    }
+};
