b1fa6f83ae3b40ca163965ffdc6d1cd46f67370f
somya-singh766 <Somyasingh3007@gmail.com>
2025-10-19 08:55:10 +0530

    Update 15.10.02.cpp

--- 15.10.02.cpp
+++ 15.10.02.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        if (!node || !node->next) {
+            return;
+        }
+        auto node_to_delete = node->next;
+        node->val = node->next->val;
+        node->next = node->next->next;
+        delete node_to_delete;
+    }
+};
