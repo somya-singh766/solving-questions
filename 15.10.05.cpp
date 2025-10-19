c32f8242b32519575b51db1e7fdab2468f7f819d
somya-singh766 <Somyasingh3007@gmail.com>
2025-10-19 09:01:54 +0530

    Update 15.10.05.cpp

--- 15.10.05.cpp
+++ 15.10.05.cpp
@@ -1,1 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        auto dummy = ListNode{0};
 
+        while (head) {
+            auto tmp = head->next;
+            head->next = dummy.next;
+            dummy.next = head;
+            head = tmp;
+        }
+
+        return dummy.next;
+    }
+};
