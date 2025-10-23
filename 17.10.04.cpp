7d5bf4aaf0b77426a3668c49886282075daee333
somya-singh766 <Somyasingh3007@gmail.com>
2025-10-23 17:06:53 +0530

    Update 17.10.04.cpp

--- 17.10.04.cpp
+++ 17.10.04.cpp
@@ -1,1 +1,21 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
 
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        long long left = 1, right = n;  // use long long to avoid overflow
+        
+        while (left < right) {
+            long long mid = left + (right - left) / 2;
+            
+            if (isBadVersion(mid)) {
+                right = mid;  // first bad version is mid or before
+            } else {
+                left = mid + 1; // first bad version is after mid
+            }
+        }
+        
+        return left; 
+    }
+};
